505bc74c1ef4a1783f7036fecf0138c965aa0496
gonnabe88 <gonnabe88@hanmail.net>
2013-08-22 19:43:53 +0900

    130822
    reactor, dispatcher

---
+++ reactor/src/ReceiveProtocol.java
@@ -0,0 +1,50 @@
+import java.net.*;   // for Socket
+import java.io.*;    // for IOException and Input/OutputStream
+import java.util.*;  // for ArrayList
+
+class ReceiveProtocol implements Runnable {
+  static public final int BUFSIZE = 32;   // Size (in bytes) of I/O buffer
+
+  private Socket clntSock;  // Connection socket
+  private Logger logger;    // Logging facility
+
+  public ReceiveProtocol(Socket clntSock, Logger logger) {
+    this.clntSock = clntSock;
+    this.logger = logger;
+  }
+
+  public void run() {
+    ArrayList entry = new ArrayList();
+    entry.add("Client address and port = " +
+      clntSock.getInetAddress().getHostAddress() + ":" +
+      clntSock.getPort());
+    entry.add("Thread = " + Thread.currentThread().getName());
+
+    try {
+      // Get the input and output I/O streams from socket
+      InputStream in = clntSock.getInputStream();
+      //OutputStream out = clntSock.getOutputStream();
+
+      int recvMsgSize;                        // Size of received message
+      int totalBytesEchoed = 0;               // Bytes received from client
+      byte[] echoBuffer = new byte[BUFSIZE];  // Receive Buffer
+      // Receive until client closes connection, indicated by -1
+      while ((recvMsgSize = in.read(echoBuffer)) != -1) {
+//        out.write(echoBuffer, 0, recvMsgSize);
+        totalBytesEchoed += recvMsgSize;
+      }
+      System.out.println("Client said; " + new String(echoBuffer) );
+      System.out.println("Client finished; received " + totalBytesEchoed + " bytes.");
+    } catch (IOException e) {
+    	System.out.println("Exception = " +  e.getMessage());
+    }
+
+    try {  // Close socket
+      clntSock.close();
+    } catch (IOException e) {
+    	System.out.println("Exception = " +  e.getMessage());
+    }
+
+    logger.writeEntry(entry);
+  }
+}
